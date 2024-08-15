01b5e4a7fe4bbd4764da96fb2f9538f717c4060a
Hano2001 <haraldnorlin93@gmail.com>
2024-08-15 16:30:05 +0200

    add hover effect on buttons

--- src/app/_components/add-location-form.tsx
+++ src/app/_components/add-location-form.tsx
@@ -55,7 +55,7 @@ export default function AddLocationForm({
   return (
     <div className="w-full md:w-1/4  h-full md:h-[800px] absolute md:top-12 md:right-12 z-[1500] bg-white rounded-md p-3">
       <button
-        className="text-white bg-red-500 w-16 rounded-md p-1"
+        className="text-white bg-red-500 hover:bg-red-400 w-16 rounded-md p-1"
         onClick={() => setShowForm(false)}
       >
         X
@@ -123,7 +123,10 @@ export default function AddLocationForm({
           })}
         </div>
         <div className="flex justify-center items-center md:justify-end w-full p-4">
-          <button className="p-3 text-white bg-green-600" type="submit">
+          <button
+            className="p-3 text-white bg-green-500 hover:bg-green-400"
+            type="submit"
+          >
             Add Location
           </button>
         </div>
