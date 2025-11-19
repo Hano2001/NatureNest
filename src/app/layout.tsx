e363ba544997f0ae87dc1f3041fad128c0a6f492
Hano2001 <haraldnorlin93@gmail.com>
2025-11-19 17:26:39 +0100

    change display height

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} h-screen relative`}>{children}</body>
+      <body className={`${inter.className} h-dvh relative overflow-hidden`}>
+        {children}
+      </body>
     </html>
   );
 }
